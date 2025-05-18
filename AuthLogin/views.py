4a5b31c5dfff670bda72bbae44400e55d2253283
Luisa <110619581+Louismttr@users.noreply.github.com>
2025-05-18 01:39:28 -0600

    Login... in process

---
+++ AuthLogin/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('Pages/home.html')  
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    
+    return render(request, 'Pages/Login.html')
